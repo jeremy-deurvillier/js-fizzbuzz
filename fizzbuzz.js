772d01ae580f0aa9d5e5c30b8bbf851b1cf793d1
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-06-19 11:34:58 +0200

    done !

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,12 +4,33 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-'use strict';
+//'use strict';
 
 var fizzbuzz = function (x) {
 //
 // YOUR CODE GOES HERE
 //
+    let result = x;
+
+    // Multiple de 3
+    if (x % 3 === 0 && x % 5 !== 0) {
+        result = 'fizz';
+    }
+
+    // Multiple de 5
+    if (x % 5 === 0 && x % 3 !== 0) {
+        result = 'buzz';
+    }
+
+    // Multiple de 3 ET 5
+    if (x % 3 === 0 && x % 5 === 0) {
+        result = 'fizzbuzz';
+    }
+    
+    
+    return result;
+
+    //return [is3Multiple, is5Multiple, bothMultiple];
 };
 
-module.exports = { fizzbuzz: fizzbuzz };
+//module.exports = { fizzbuzz: fizzbuzz };
---
+++ main.js
@@ -0,0 +1,5 @@
+console.log(fizzbuzz(2));
+console.log(fizzbuzz(5));
+console.log(fizzbuzz(6));
+console.log(fizzbuzz(11));
+console.log(fizzbuzz(30));
